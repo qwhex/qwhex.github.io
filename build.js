5ded70994d066d2d591ec9fbe03c12873ad6e223
Mice Pápai <mice@gorbekor.hu>
2017-06-25 12:56:44 +0200

    Initial commit

---
+++ build.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const pug = require('pug')
+const package_json = require('./package.json')
+
+function compile_page() {
+  console.info('compiling pug...')
+
+  const compile_index = pug.compileFile('pug/index.pug')
+  const index = compile_index({
+    email: 'hello@micepapai.com',
+    revised: Date(),
+    revision: package_json.version,
+  })
+  fs.writeFileSync('index.html', index)
+}
+
+if (require.main === module) {
+  compile_page()
+}
