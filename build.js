79648f9b25724be7dde1190d55a5514b6cdc3922
Mice Pápai <hello@micepapai.com>
2022-07-17 22:59:10 +0200

    2022-07 update

--- build.js
+++ build.js
@@ -3,15 +3,21 @@ const pug = require('pug')
 const package_json = require('./package.json')
 
 function compile_page() {
-  console.info('compiling pug...')
+  console.info('⚙️  compiling pugs...')
 
-  const compile_index = pug.compileFile('pug/index.pug')
+  const compile_index = pug.compileFile('pug/index.pug', {basedir: 'pug'})
+
   const index = compile_index({
     email: 'hello@micepapai.com',
+    themeColor: '#f71559',
     revised: Date(),
     revision: package_json.version,
+    cssPath: `css/q.min.css?${Date.now()}`,
   })
+
   fs.writeFileSync('index.html', index)
+
+  console.info('✨ compiled pugs')
 }
 
 if (require.main === module) {
